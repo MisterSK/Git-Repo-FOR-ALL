b8392d1076afc721f69064ab7ab00807e3de2e0c
Sanya Khasenye <MisterSK@users.noreply.github.com>
2024-06-05 16:27:00 +0100

    Adding lesson content

--- Git-Workflow-Demo.ps1
+++ Git-Workflow-Demo.ps1
@@ -1,3 +1,6 @@
 $timestamp = Get-Date
 $timestamp = $timestamp.ToString("dddd dd/MM/yyyy HH:mm")
 Write-Host "It is $timestamp"
+Write-Host ""
+Write-Host "-------------------"
+Write-Host "Lesson: Introduction to Git, GitHub"
